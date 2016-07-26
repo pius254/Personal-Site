3e9ec8f17f90417e70fcfd8ed7d919854c0c3629
Pius Wanjiru <piusmuriithi1@gmail.com>
2016-07-26 16:56:37 +0300

    adds smooth scroll on the document

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,19 @@ var wow = new WOW({
     mobile: false
 });
 wow.init();
+
+//ScrollTop
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
