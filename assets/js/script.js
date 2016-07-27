408e63aaa09be30c0017cafe4a07e89d50a48e7f
Pius Wanjiru <piusmuriithi1@gmail.com>
2016-07-27 08:55:17 +0300

    adds more smooth scroll on the document-on the navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+$('body').scrollspy({ target: '.navmenu' });
+
+// When we click on the LI
+$("ul.qcontrols li").click(function(){
+  // If this isn't already active
+  if (!$(this).hasClass("active")) {
+    // Remove the class from anything that is active
+    $("ul.qcontrols li.active").removeClass("active");
+    // And make this active
+    $(this).addClass("active");
+  }
+});
+
+
 //WOW Scroll Spy
 var wow = new WOW({
     //disabled for mobile
